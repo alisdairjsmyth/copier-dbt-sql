c3361d9692603823fd6e69148fb619c143a3097c
alisdairjsmyth <alisdair.smyth@gmail.com>
2026-01-11 18:12:31 +1100

    feat: add generate_schema_name override
    Override the default dbt schema naming logic to use absolute values from either the profile or in model configuration.

---
+++ template/src/macros/overrides/generate_schema_name.sql
@@ -0,0 +1,10 @@
+{% macro generate_schema_name(custom_schema_name, node) -%}
+    {%- set default_schema = target.schema -%}
+
+    {%- if custom_schema_name is not null -%}
+        {{ custom_schema_name | trim }}
+
+    {%- else -%}
+        {{ default_schema }}
+    {%- endif -%}
+{%- endmacro %}
